e43f711c0f2edac4a9769f701cfbe878f95ae1f7
MJCappella <mjaymickey001@gmail.com>
2024-07-08 22:55:47 +0300

    products model

---
+++ app/Models/ProductsModel.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ProductsModel extends Model
+{
+    use HasFactory;
+
+    protected $fillable=[
+        'productname',
+        'productquantity',
+        'productprice',
+        'productdescription',
+    ];
+}
