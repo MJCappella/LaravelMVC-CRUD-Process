0b1c09ccaabee952943cd622d4d559dded5ba19f
MJCappella <mjaymickey001@gmail.com>
2024-07-09 01:27:01 +0300

    products controller

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -3,8 +3,44 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\ProductsModel;
 
 class ProductController extends Controller
 {
-    //
+    public function index()
+    {
+        return view('products.index');
+    }
+
+    public function create()
+    {
+        return view('products.create');
+    }
+
+    public function edit()
+    {
+        return view('products.edit');
+    }
+
+    public function delete()
+    {
+        return view('products.delete');
+    }
+
+    public function store(Request $request)
+    {
+        $data = $request -> validate(
+            [
+                'productname' => 'required',
+                'quantity' => 'required|numeric',
+                'price' => 'required|numeric',
+                'description' => 'required',
+            ]
+            );
+
+        $newproduct = ProductsModel::create($data);
+
+        return redirect(route('products.index'));
+    }
+
 }
