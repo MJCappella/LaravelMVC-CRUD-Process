89499bbcfc4b5145add4d1405570af2c60944019
MJCappella <mjaymickey001@gmail.com>
2024-07-08 23:00:24 +0300

    migration update

--- database/migrations/2024_07_08_140948_create_products_table.php
+++ database/migrations/2024_07_08_140948_create_products_table.php
@@ -13,7 +13,7 @@ class CreateProductsTable extends Migration
      */
     public function up()
     {
-        Schema::create('products', function (Blueprint $table) {f
+        Schema::create('products', function (Blueprint $table) {
             $table->id();
             $table->string('productname');
             $table->integer('productquantity');
