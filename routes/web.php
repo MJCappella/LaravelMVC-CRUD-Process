3cab1c8944216bdb4b5fea2040aff462bcdceb74
MJCappella <mjaymickey001@gmail.com>
2024-07-09 01:27:29 +0300

    api and route building

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+//CRUD routes for the products section
+Route::get('/products', [ProductController::class, 'index'])->name('products.index');
+Route::get('/products/create', [ProductController::class, 'create'])->name('products.create');
+Route::get('/products/delete', [ProductController::class, 'delete'])->name('product.delete');
+Route::get('/products/edit', [ProductController::class, 'edit'])->name('products.edit');
+
+//storing data from the form into the db
+Route::post('/products/store', [ProductController::class, 'store'])->name('products.store');
